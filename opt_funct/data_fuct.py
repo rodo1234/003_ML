2928d528f37faf8bc80f70a0acf94ef273443660
Ricardo <if722544@iteso.mx>
2024-03-09 16:11:11 -0600

    se comienza proceso de optimizacion con combinaciones posibles

--- opt_funct/data_fuct.py
+++ opt_funct/data_fuct.py
@@ -20,3 +20,14 @@ class functions:
         # df['Y_SELL'] = df['Y_SELL'].astype(int)
 
         return df
+
+class Operation:
+    def __init__(self, operation_type, bought_at, timestamp, n_shares,
+                 stop_loss, take_profit):
+        self.operation_type = operation_type
+        self.bought_at = bought_at
+        self.timestamp = timestamp
+        self.n_shares = n_shares
+        self.sold_at = None
+        self.stop_loss = stop_loss
+        self.take_profit = take_profit
